c72ab9b70d0d548ffff77b10651a9dfa70060248
Chris Keating <christopherkeating@gmail.com>
2013-02-22 23:03:12 -0600

    Update javascripts/drawing.js

--- javascripts/drawing.js
+++ javascripts/drawing.js
@@ -57,3 +57,53 @@ function pacman(ctx, scale, step) {
     ctx.closePath();
     ctx.restore();
 }
+function ghost(ctx, size, color, step) {
+    var steps = [
+        {points: [[1, 0], [0.85, -0.25], [0.68, 0], [0.50, -0.25], [0.32, 0], [0.15, -0.25], [0, 0]]},
+        {points: [[0.85, 0], [0.68, -0.25], [0.50, 0], [0.32, -0.25], [0.15, 0], [0,-0.25]]},
+    ];
+    var this_step = steps[step%steps.length];
+    ctx.save();
+    ctx.translate(-size/2, size/2);
+    /*
+     ctx.beginPath();
+     ctx.moveTo(0,0);
+     ctx.arc(0, 0, 5, 0, 2*Math.PI, true);
+     ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(0,-size);
+    ctx.lineTo(size, -size);
+    ctx.stroke();
+     */
+    ctx.beginPath();
+    ctx.moveTo(0,size*-0.25);
+    var arcStart = -size*0.75;
+    ctx.lineTo(0, arcStart);
+    ctx.strokeStyle='black';
+    ctx.lineWidth = 5;
+    ctx.quadraticCurveTo(size *0.15, -size, size/2, -size);
+    ctx.quadraticCurveTo(size *0.85, -size, size, arcStart);
+    ctx.lineTo(size, size*-0.25);
+    this_step.points.forEach(function(point){
+        ctx.lineTo(size*point[0], size*point[1]);
+    });
+    /*
+    ctx.lineTo(size*0.85, -size*0.25);
+    ctx.lineTo(size*0.68, 0);
+    ctx.lineTo(size*0.50, -size*0.25);
+    ctx.lineTo(size*0.32, 0);
+    ctx.lineTo(size*0.15, -size*0.25);
+    */
+    ctx.fillStyle = color;
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.strokeStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(size*0.15, -size*0.7, size*0.2, size*0.2);
+    ctx.fillRect(size*0.65, -size*0.7, size*0.2, size*0.2);
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(size*0.15, -size*0.7, size*0.1, size*0.1);
+    ctx.fillRect(size*0.65, -size*0.7, size*0.1, size*0.1);
+    ctx.restore();
+}
